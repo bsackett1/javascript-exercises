671abeb7e4e5770e95ecdb2d1bd93b8f7346cb47
Brendan Sackett <brendansackett@gmail.com>
2025-11-21 09:37:10 -0500

    Complete object exersizes

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  return people.reduce((person, curr) =>
+    getAge(person) > getAge(curr) ? person : curr
+  )
+}
 
-};
+function getAge (person) {
+  return person.yearOfDeath
+    ? person.yearOfDeath - person.yearOfBirth
+    : thisYear - person.yearOfBirth
+}
+
+const thisYear = new Date().getFullYear()
 
 // Do not edit below this line
-module.exports = findTheOldest;
+module.exports = findTheOldest
